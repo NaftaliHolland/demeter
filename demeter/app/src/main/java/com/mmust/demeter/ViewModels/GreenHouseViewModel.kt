0485342959dc487000c5578206614846f0dc495b
Naftali Holland <naftaliholland01@gmail.com>
2024-11-25 23:47:35 +0300

    Replassed mock api to fetch database with firebase firestore

--- demeter/app/src/main/java/com/mmust/demeter/ViewModels/GreenHouseViewModel.kt
+++ demeter/app/src/main/java/com/mmust/demeter/ViewModels/GreenHouseViewModel.kt
@@ -3,11 +3,13 @@ package com.mmust.demeter.ViewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.FirebaseAuth
 import com.mmust.demeter.Models.GreenHouse
 import com.mmust.demeter.network.GreenHouseApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class GreenHouseViewModel : ViewModel() {
     private val _greenHouses: MutableStateFlow<List<GreenHouse>> = MutableStateFlow(listOf())
@@ -19,16 +21,39 @@ class GreenHouseViewModel : ViewModel() {
     private val _error: MutableStateFlow<String?> = MutableStateFlow(null)
     val error: StateFlow<String?> = _error
 
+    private val firestore = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+
     init {
         getGreenHouses()
     }
 
     private fun getGreenHouses() {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            fetchGreenHouses(userId)
+        } else {
+            _error.value = "User not authenticated"
+        }
+    }
+    private fun fetchGreenHouses(userId: String) {
         viewModelScope.launch {
             _isLoading.value = true
             try {
-                val greenHouses = GreenHouseApi.apiService.getGreenHouses()
+                val snapshot = firestore.collection("users")
+                    .document(userId)
+                    .collection("greenhouses")
+                    .get()
+                    .await()
+                val greenHousesJson = snapshot.documents.map { document ->
+                    document.data
+                }
+                println(greenHousesJson)
+
+                val greenHouses = snapshot.toObjects(GreenHouse::class.java)
                 _greenHouses.value = greenHouses
+
             } catch (e: Exception) {
                 _error.value = e.message
             } finally {
@@ -36,5 +61,4 @@ class GreenHouseViewModel : ViewModel() {
             }
         }
     }
-
 }
